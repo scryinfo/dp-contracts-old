2928a0551480e2b6b4b2268782d6330dd28cfd2c
Abhishek Kumar <bettiah@gmail.com>
2017-11-29 10:09:28 +0800

    added watch

--- server.py
+++ server.py
@@ -1,122 +1,138 @@
-from flask import Flask, request, jsonify, abort
-from http import HTTPStatus
-from populus import Project
 import binascii
+from flask import Flask, request, jsonify, current_app
+from populus import Project
 from populus.utils.wait import wait_for_transaction_receipt
 
 
-app = Flask('app')
-project = Project()
+def run_app(app):
+    project = Project()
+    with project.get_chain('scrychain') as chain:
+        def on_transfer(args):
+            print(f"new transfer: {args}")
 
-with project.get_chain('scrychain') as chain:
-    # accounts need to be unlocked
-    (owner, buyer, seller, verifier) = chain.web3.eth.accounts[:4]
-    accounts = {}
-    accounts['owner'] = owner
-    accounts['buyer'] = buyer
-    accounts['seller'] = seller
-    accounts['verifier'] = verifier
+        def on_channel(args):
+            print(f"new channel: {args}")
 
-    print(f"accounts: {accounts}")
+        def on_settle(args):
+            print(f"new settlement: {args}")
 
-    token, _ = chain.provider.get_or_deploy_contract(
-        'ScryToken',
-        deploy_args=[1000],
-        deploy_transaction={'from': owner})
-    print(f"token: {token.address}")
+        # accounts need to be unlocked
+        (owner, buyer, seller, verifier) = chain.web3.eth.accounts[:4]
+        accounts = {}
+        accounts['owner'] = owner
+        accounts['buyer'] = buyer
+        accounts['seller'] = seller
+        accounts['verifier'] = verifier
 
-    contract, _ = chain.provider.get_or_deploy_contract(
-        'Scry',
-        deploy_args=[token.address],
-        deploy_transaction={'from': owner})
-    print(f"contract: {contract.address}")
-    accounts['contract'] = contract.address
+        print(f"accounts: {accounts}")
 
-    # check balance
-    @app.route('/balance')
-    def balance():
-        account = accounts[request.args.get('account')]
-        response = jsonify({'balance': token.call().balanceOf(account)})
-        return response
+        token, _ = chain.provider.get_or_deploy_contract(
+            'ScryToken',
+            deploy_args=[1000],
+            deploy_transaction={'from': owner})
+        print(f"token: {token.address}")
 
-    # fund participant
-    @app.route('/fund')
-    def fund():
-        to = request.args.get('account')
-        account = accounts[to]
-        amount = int(request.args.get('amount'))
-        print(f"fund {to}: {amount}")
+        token.on('Transfer', {}, on_transfer)
 
-        txid = token.transact({"from": owner}).transfer(account, amount)
-        print(f"buyer fund txid: {txid}")
-        receipt = wait_for_transaction_receipt(chain.web3, txid)
-        print(f"buyer receipt: {receipt}")
-        response = jsonify({'balance': token.call().balanceOf(buyer)})
-        return response
+        contract, _ = chain.provider.get_or_deploy_contract(
+            'Scry',
+            deploy_args=[token.address],
+            deploy_transaction={'from': owner})
+        print(f"contract: {contract.address}")
+        accounts['contract'] = contract.address
 
-    # create channel to seller
-    @app.route('/buyer/channel')
-    def channel():
-        amount = int(request.args.get('amount', 100))
-        print(f"channel: {amount}")
-        txid = token.transact({"from": buyer}).transfer(
-            contract.address, amount, bytes.fromhex(seller[2:].zfill(40)))
-        print("channel txid:" + txid)
-        receipt = wait_for_transaction_receipt(chain.web3, txid)
-        print(f"channel receipt: {receipt}")
-        response = jsonify({'create_block': receipt['blockNumber']})
-        return response
+        contract.on('ChannelCreated', {}, on_channel)
+        contract.on('ChannelSettled', {}, on_settle)
 
-    # authorize string
-    @app.route('/buyer/authorize')
-    def authorize():
-        amount = int(request.args.get('amount', 100))
-        create_block = int(request.args.get('create_block'))
-        msg = contract.call().getBalanceMessage(seller, create_block, amount)
-        response = jsonify(
-            {'balance_sig': chain.web3.eth.sign(buyer, msg)[2:]})
-        return response
+        # check balance
+        @app.route('/balance')
+        def balance():
+            account = accounts[request.args.get('account')]
+            response = jsonify({'balance': token.call().balanceOf(account)})
+            return response
 
-    #
-    @app.route('/verifier/sign')
-    def verify():
-        cid = request.args.get('CID')
-        # verifier does its thing
-        verification = contract.call().getVerifyMessage(seller, cid)
-        response = jsonify(
-            {'verification_sig': chain.web3.eth.sign(verifier, verification)[2:]})
-        return response
+        # fund participant
+        @app.route('/fund')
+        def fund():
+            to = request.args.get('account')
+            account = accounts[to]
+            amount = int(request.args.get('amount'))
 
-    @app.route("/seller/verify_balance")
-    def verify_balance():
-        amount = int(request.args.get('amount', 100))
-        balance_sig = request.args.get('balance_sig')
-        create_block = int(request.args.get('create_block'))
-        msg = contract.call().getBalanceMessage(seller, create_block, amount)
-        print(f"msg: {msg}")
-        proof = contract.call().verifyBalanceProof(
-            seller, create_block, amount, binascii.unhexlify(balance_sig))
-        print(f"proof: {proof}")
-        if(proof.lower() == buyer):
-            response = jsonify({'verification': 'OK'})
-        else:
-            response = jsonify({'verification': "!!"})
-        return response
+            txid = token.transact({"from": owner}).transfer(account, amount)
+            print(f"buyer fund {to}: {amount} txid: {txid}")
+            receipt = wait_for_transaction_receipt(chain.web3, txid)
+            print(f"buyer receipt: {receipt}")
+            response = jsonify({'balance': token.call().balanceOf(buyer)})
+            return response
 
-    @app.route('/seller/close')
-    def close():
-        amount = int(request.args.get('amount', 100))
-        balance_sig = request.args.get('balance_sig')
-        verify_sig = request.args.get('verification_sig')
-        cid = request.args.get('CID')
-        create_block = int(request.args.get('create_block'))
+        # create channel to seller
+        @app.route('/buyer/channel')
+        def channel():
+            amount = int(request.args.get('amount', 100))
+            txid = token.transact({"from": buyer}).transfer(
+                contract.address, amount, bytes.fromhex(seller[2:].zfill(40)))
+            print(f"channel amount {amount} txid: {txid}")
+            receipt = wait_for_transaction_receipt(chain.web3, txid)
+            print(f"channel receipt: {receipt}")
+            response = jsonify({'create_block': receipt['blockNumber']})
+            return response
 
-        txid = contract.transact({"from": seller}).close(
-            buyer, create_block, amount, binascii.unhexlify(balance_sig), verifier, cid, binascii.unhexlify(verify_sig))
-        print(f"close txid: {txid}")
-        receipt = wait_for_transaction_receipt(chain.web3, txid)
-        print(f"close receipt: {receipt}")
-        response = jsonify({'close_block': receipt['blockNumber']})
-        return response
+        # authorize string
+        @app.route('/buyer/authorize')
+        def authorize():
+            amount = int(request.args.get('amount', 100))
+            create_block = int(request.args.get('create_block'))
+            msg = contract.call().getBalanceMessage(seller, create_block, amount)
+            response = jsonify(
+                {'balance_sig': chain.web3.eth.sign(buyer, msg)[2:]})
+            return response
 
-    app.run(use_reloader=True, threaded=False, debug=True)
+        #
+        @app.route('/verifier/sign')
+        def verify():
+            cid = request.args.get('CID')
+            # verifier does its thing
+            verification = contract.call().getVerifyMessage(seller, cid)
+            response = jsonify(
+                {'verification_sig': chain.web3.eth.sign(verifier, verification)[2:]})
+            return response
+
+        @app.route("/seller/verify_balance")
+        def verify_balance():
+            amount = int(request.args.get('amount', 100))
+            balance_sig = request.args.get('balance_sig')
+            create_block = int(request.args.get('create_block'))
+            msg = contract.call().getBalanceMessage(seller, create_block, amount)
+            print(f"msg: {msg}")
+            proof = contract.call().verifyBalanceProof(
+                seller, create_block, amount, binascii.unhexlify(balance_sig))
+            print(f"proof: {proof}")
+            if(proof.lower() == buyer):
+                response = jsonify({'verification': 'OK'})
+            else:
+                response = jsonify({'verification': "!!"})
+            return response
+
+        @app.route('/seller/close')
+        def close():
+            amount = int(request.args.get('amount', 100))
+            balance_sig = request.args.get('balance_sig')
+            verify_sig = request.args.get('verification_sig')
+            cid = request.args.get('CID')
+            create_block = int(request.args.get('create_block'))
+
+            txid = contract.transact({"from": seller}).close(
+                buyer, create_block, amount, binascii.unhexlify(balance_sig), verifier, cid, binascii.unhexlify(verify_sig))
+            print(f"close txid: {txid}")
+            receipt = wait_for_transaction_receipt(chain.web3, txid)
+            print(f"close receipt: {receipt}")
+            response = jsonify({'close_block': receipt['blockNumber']})
+            return response
+
+
+flask = Flask(__name__)
+with flask.app_context():
+    run_app(current_app)
+
+if __name__ == "__main__":
+    flask.run(threaded=False, debug=True)

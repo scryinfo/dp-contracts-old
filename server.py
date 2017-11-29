886624bd63a3061e04c2aae6d7056b7aa2549d02
Abhishek Kumar <bettiah@gmail.com>
2017-11-29 17:49:19 +0800

    better txn error handling, use Byzantium's status field

--- server.py
+++ server.py
@@ -1,12 +1,41 @@
 import binascii
+import sys
 from flask import Flask, request, jsonify, current_app
 from populus import Project
 from populus.utils.wait import wait_for_transaction_receipt
 
 
+class TransactionFailed(Exception):
+    status_code = 400
+
+    def __init__(self, gas, gasUsed):
+        Exception.__init__(self)
+
+
+def check_txn(chain, txid):
+    receipt = wait_for_transaction_receipt(chain.web3, txid)
+    print(f"receipt: {receipt}")
+    # post BZ : status is 1 for success
+    if receipt.status == '0x1':
+        return receipt
+    # 0 for fail with REVERT (for THROW gasused == gas)
+    txinfo = chain.web3.eth.getTransaction(txid)
+    print(f"fund txn: {txinfo}")
+    raise TransactionFailed(txinfo['gas'], receipt['gasUsed'])
+
+
 def run_app(app):
-    project = Project()
-    with project.get_chain('scrychain') as chain:
+
+    @app.errorhandler(TransactionFailed)
+    def transaction_failed(error):
+        message = {
+            'message': 'Transaction Failed',
+        }
+        resp = jsonify(message)
+        resp.status_code = error.status_code
+        return resp
+
+    with Project().get_chain('scrychain') as chain:
         def on_transfer(args):
             print(f"new transfer: {args}")
 
@@ -17,12 +46,16 @@ def run_app(app):
             print(f"new settlement: {args}")
 
         # accounts need to be unlocked
-        (owner, buyer, seller, verifier) = chain.web3.eth.accounts[:4]
         accounts = {}
-        accounts['owner'] = owner
-        accounts['buyer'] = buyer
-        accounts['seller'] = seller
-        accounts['verifier'] = verifier
+        try:
+            (owner, buyer, seller, verifier) = chain.web3.eth.accounts[:4]
+            accounts['owner'] = owner
+            accounts['buyer'] = buyer
+            accounts['seller'] = seller
+            accounts['verifier'] = verifier
+        except ConnectionRefusedError as e:
+            print(f"Cannot connect to geth: {e}", file=sys.stderr)
+            raise
 
         print(f"accounts: {accounts}")
 
@@ -59,11 +92,8 @@ def run_app(app):
             amount = int(request.args.get('amount'))
 
             txid = token.transact({"from": owner}).transfer(account, amount)
-            print(f"buyer fund {to}: {amount} txid: {txid}")
-            receipt = wait_for_transaction_receipt(chain.web3, txid)
-            print(f"buyer receipt: {receipt}")
-            response = jsonify({'balance': token.call().balanceOf(buyer)})
-            return response
+            check_txn(chain, txid)
+            return jsonify({'balance': token.call().balanceOf(buyer)})
 
         # create channel to seller
         @app.route('/buyer/channel')
@@ -72,10 +102,8 @@ def run_app(app):
             txid = token.transact({"from": buyer}).transfer(
                 contract.address, amount, bytes.fromhex(seller[2:].zfill(40)))
             print(f"channel amount {amount} txid: {txid}")
-            receipt = wait_for_transaction_receipt(chain.web3, txid)
-            print(f"channel receipt: {receipt}")
-            response = jsonify({'create_block': receipt['blockNumber']})
-            return response
+            receipt = check_txn(chain, txid)
+            return jsonify({'create_block': receipt['blockNumber']})
 
         # authorize string
         @app.route('/buyer/authorize')
@@ -83,19 +111,17 @@ def run_app(app):
             amount = int(request.args.get('amount', 100))
             create_block = int(request.args.get('create_block'))
             msg = contract.call().getBalanceMessage(seller, create_block, amount)
-            response = jsonify(
+            return jsonify(
                 {'balance_sig': chain.web3.eth.sign(buyer, msg)[2:]})
-            return response
 
-        #
+        # verification string
         @app.route('/verifier/sign')
         def verify():
             cid = request.args.get('CID')
             # verifier does its thing
             verification = contract.call().getVerifyMessage(seller, cid)
-            response = jsonify(
+            return jsonify(
                 {'verification_sig': chain.web3.eth.sign(verifier, verification)[2:]})
-            return response
 
         @app.route("/seller/verify_balance")
         def verify_balance():
@@ -121,13 +147,16 @@ def run_app(app):
             cid = request.args.get('CID')
             create_block = int(request.args.get('create_block'))
 
-            txid = contract.transact({"from": seller}).close(
-                buyer, create_block, amount, binascii.unhexlify(balance_sig), verifier, cid, binascii.unhexlify(verify_sig))
-            print(f"close txid: {txid}")
-            receipt = wait_for_transaction_receipt(chain.web3, txid)
-            print(f"close receipt: {receipt}")
-            response = jsonify({'close_block': receipt['blockNumber']})
-            return response
+            txid = contract.transact({"from": seller}).close(buyer,
+                                                             create_block,
+                                                             amount,
+                                                             binascii.unhexlify(
+                                                                 balance_sig),
+                                                             verifier,
+                                                             cid,
+                                                             binascii.unhexlify(verify_sig))
+            receipt = check_txn(chain, txid)
+            return jsonify({'close_block': receipt['blockNumber']})
 
 
 flask = Flask(__name__)

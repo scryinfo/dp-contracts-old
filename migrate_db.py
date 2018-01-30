29b92f9242fd1787df6e03c1fb8be64ec5f03dbf
Abhishek Kumar <bettiah@gmail.com>
2018-01-30 15:08:27 +0800

    migrate script

---
+++ migrate_db.py
@@ -0,0 +1,8 @@
+from playhouse.migrate import *
+db = SqliteDatabase('scry.db')
+
+migrator = SqliteMigrator(db)
+with db.transaction():
+    migrate(
+        migrator.drop_column('trader', 'password')
+    )

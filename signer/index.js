7285b3663e68c59ee11f9b6937ced216562f237f
Abhishek Kumar <bettiah@gmail.com>
2018-02-16 23:32:04 +0000

    openchannel works

--- signer/index.js
+++ signer/index.js
@@ -1,12 +1,14 @@
 "use strict";
 
+const Tx = require("ethereumjs-tx");
 const Web3 = require("web3");
+const { toHex, padLeft, hexToBytes } = require("web3-utils");
 const contracts = require("../build/contracts.json");
 const deployments = require("../registrar.json").deployments;
-const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+const provider = new Web3.providers.WebsocketProvider("ws://localhost:8546");
 const web3 = new Web3(provider);
-var Tx = require("ethereumjs-tx");
 
+// converts { "blockchain..":{"key":"val"}} to {"key":"val"}
 const registry = Object.values(deployments).reduce(
   (acc, ele) => (acc = Object.assign(acc, ele)),
   {}
@@ -17,52 +19,105 @@ const token = new web3.eth.Contract(
   registry["ScryToken"]
 );
 console.info("token:", token._address);
+const contract = new web3.eth.Contract(contracts["Scry"].abi, registry["Scry"]);
+console.info("contract:", contract._address);
 
-const privateKey =
-  "0x3686e245890c7f997766b73a21d8e59f6385e1208831af3862574790cbc3d158";
+// event subscriptions
+function onEvt(err, evt) {
+  console.info(`event: ${evt.event} : ${JSON.stringify(evt.returnValues)}`);
+}
+token.events.Transfer({}, onEvt);
+contract.events.ChannelCreated({}, onEvt);
+contract.events.ChannelSettled({}, onEvt);
 
-const acct = web3.eth.accounts.privateKeyToAccount(privateKey);
-console.info("acct:", acct.address);
+const buyerKey =
+  "0xa518652e06e57e6d9e9ff93ad5eef2892415a9735ff14c246eff301342c8e0f7";
+const buyer = web3.eth.accounts.privateKeyToAccount(buyerKey);
+// console.info("buyer:", buyer);
+
+const sellerKey =
+  "0x80ea726eaa53b661b558c9be7d752c8521719b9f018fd7c3d102020580a61d0f";
+const seller = web3.eth.accounts.privateKeyToAccount(sellerKey);
+// console.info("seller:", seller);
 
 async function main() {
   const coinbase = await web3.eth.getCoinbase();
   console.info("coinbase", coinbase);
 
-  // send from coinbase to address
-  // console.info("tokens:", await token.methods.balanceOf(acct.address).call());
-  // const receipt1 = await token.methods
-  //   .transfer(acct.address, 100)
-  //   .send({
-  //     from: coinbase
-  //   });
-  // console.info("token receipt:", receipt1);
-
-  console.info("tokens:", await token.methods.balanceOf(acct.address).call());
-
-  const nonce = await web3.eth.getTransactionCount(acct.address);
-  console.info("nonce:", nonce);
+  // await sendTokenFromCoinbase(coinbase, buyer.address);
 
   // send from address to coinbase
-  const payload = token.methods.transfer(coinbase, 100).encodeABI();
+  // await sendToken(buyer, coinbase);
+
+  await openChannel(2, buyer, seller, 10, 1);
+}
+
+async function tokenBalance(address) {
+  return await token.methods.balanceOf(address).call();
+}
+
+async function sendToken(from, to) {
+  console.info("tokens:", await tokenBalance(from.address));
+
+  const nonce = await web3.eth.getTransactionCount(from.address);
+  console.info("nonce:", nonce);
+
+  const payload = token.methods.transfer(to, 1).encodeABI();
   console.info("pld", payload);
   const tx = new Tx({
     nonce: nonce,
-    from: acct.address,
+    from: from.address,
     to: token._address,
     gas: 38121,
-    // gasPrice: web3.utils.toWei("20", "gwei"),
     data: payload
   });
-  tx.sign(Buffer.from(privateKey.slice(2), "hex"));
+  tx.sign(Buffer.from(buyer.privateKey.slice(2), "hex"));
   const signed = "0x" + tx.serialize().toString("hex");
-  // const signed = await web3.eth.signTransaction(tx, coinbase);
   console.info("signed", signed);
 
   const receipt = await web3.eth.sendSignedTransaction(signed);
-  console.info("token receipt:", receipt);
-  console.info("tokens:", await token.methods.balanceOf(acct.address).call());
+  console.info("tfr:", receipt.blockNumber, receipt.gasUsed, receipt.status);
+  console.info("tokens:", await tokenBalance(from.address));
+}
+
+// send from coinbase to address
+async function sendTokenFromCoinbase(coinbase, address) {
+  console.info("tokens:", await token.methods.balanceOf(address).call());
+  const receipt1 = await token.methods.transfer(address, 100).send({
+    from: coinbase
+  });
+  console.info("token receipt:", receipt1);
+}
+
+async function openChannel(amount, buyer, seller, reward, verifiers) {
+  console.info("buyer tokens:", await tokenBalance(buyer.address));
+  console.info("seller tokens:", await tokenBalance(seller.address));
+
+  const hx =
+    "0x" +
+    seller.address.slice(2) +
+    padLeft(reward, 8).slice(2) +
+    padLeft(verifiers, 8).slice(2);
+
+  const payload = token.methods
+    .transfer(contract._address, amount, hx)
+    .encodeABI();
+
+  const nonce = await web3.eth.getTransactionCount(buyer.address);
+  const tx = new Tx({
+    nonce: nonce,
+    from: buyer.address,
+    to: token._address,
+    gas: 198579,
+    data: payload
+  });
+  tx.sign(Buffer.from(buyer.privateKey.slice(2), "hex"));
+  const signed = "0x" + tx.serialize().toString("hex");
+  const receipt = await web3.eth.sendSignedTransaction(signed);
+  console.info("tfr:", receipt);
+  console.info("tokens:", await tokenBalance(buyer.address));
 }
 
 main().catch(error => {
-  console.error("error", error);
+  console.error(error);
 });

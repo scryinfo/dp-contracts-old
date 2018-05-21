0a84d96aa08d6d071030ee09cfdd36ea802eeba1
Abhishek Kumar <bettiah@gmail.com>
2018-05-21 23:30:02 +0800

    removed v1 backend

--- migrate_db.py
+++
@@ -1,8 +0,0 @@
-from playhouse.migrate import *
-db = SqliteDatabase('scry.db')
-
-migrator = SqliteMigrator(db)
-with db.transaction():
-    migrate(
-        migrator.drop_column('purchaseorder', 'verifiers')
-    )
